a5b59a3ee838ce556c435d74e05f4a2ce1f2178b
Roger Bleggi <roger.bleggi@hotmail.com>
2025-06-28 02:01:15 -0300

    feat: implement Converter trait and example usage for address and person types

---
+++ scala-3/converter-framework/src/main/scala/com/rbleggi/converter-framework/main.scala
@@ -0,0 +1,32 @@
+package converter.framework
+
+trait Converter[A, B] {
+  def convert(a: A): B
+}
+
+object Converter {
+  def apply[A, B](f: A => B): Converter[A, B] = new Converter[A, B] {
+    def convert(a: A): B = f(a)
+  }
+}
+
+@main def runRestaurantQueue(): Unit =
+  val queue = KitchenQueue()
+
+// Example complex types
+case class Address(street: String, city: String, zip: String)
+case class Person(name: String, age: Int, address: Address)
+case class PersonDTO(fullName: String, age: Int, city: String)
+
+// Example converters
+val addressToString: Converter[Address, String] = Converter(addr => s"${addr.street}, ${addr.city}, ${addr.zip}")
+val personToDTO: Converter[Person, PersonDTO] = Converter(p => PersonDTO(p.name, p.age, p.address.city))
+val personToString: Converter[Person, String] = Converter(p => s"${p.name} (${p.age}), ${addressToString.convert(p.address)}")
+
+@main def runConverterExamples(): Unit =
+  val address = Address("123 Main St", "Springfield", "12345")
+  val person = Person("John Doe", 30, address)
+
+  println("Address as String: " + addressToString.convert(address))
+  println("Person as DTO: " + personToDTO.convert(person))
+  println("Person as String: " + personToString.convert(person))
