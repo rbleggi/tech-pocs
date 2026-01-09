cee6994f25e314b292157319eb867a04fa1bd71f
Roger Bleggi <roger.bleggi@hotmail.com>
2026-01-08 21:12:33 -0300

    feat: add cosine similarity metric

--- scala-3/recommendation-system/src/main/scala/com/rbleggi/recommendationsystem/RecommendationSystem.scala
+++ scala-3/recommendation-system/src/main/scala/com/rbleggi/recommendationsystem/RecommendationSystem.scala
@@ -9,3 +9,13 @@ case class Recommendation(itemId: String, score: Double, reason: String)
 trait RecommendationStrategy:
   def recommend(userId: String, ratings: List[Rating], items: List[Item], topN: Int = 5): List[Recommendation]
   def name: String
+
+object SimilarityMetrics:
+  def cosineSimilarity(v1: Map[String, Double], v2: Map[String, Double]): Double =
+    val allKeys = (v1.keys ++ v2.keys).toSet
+    val dotProduct = allKeys.map(k => v1.getOrElse(k, 0.0) * v2.getOrElse(k, 0.0)).sum
+    val mag1 = Math.sqrt(allKeys.map(k => Math.pow(v1.getOrElse(k, 0.0), 2)).sum)
+    val mag2 = Math.sqrt(allKeys.map(k => Math.pow(v2.getOrElse(k, 0.0), 2)).sum)
+
+    if mag1 == 0.0 || mag2 == 0.0 then 0.0
+    else dotProduct / (mag1 * mag2)
