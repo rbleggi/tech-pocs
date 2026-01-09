b1c648384d43e9d9cf2d04668063786517a651d9
Roger Bleggi <roger.bleggi@hotmail.com>
2026-01-08 21:12:12 -0300

    feat: add recommendation strategy trait

--- scala-3/recommendation-system/src/main/scala/com/rbleggi/recommendationsystem/RecommendationSystem.scala
+++ scala-3/recommendation-system/src/main/scala/com/rbleggi/recommendationsystem/RecommendationSystem.scala
@@ -5,3 +5,7 @@ case class Rating(userId: String, itemId: String, score: Double)
 case class Item(id: String, features: Map[String, Double])
 
 case class Recommendation(itemId: String, score: Double, reason: String)
+
+trait RecommendationStrategy:
+  def recommend(userId: String, ratings: List[Rating], items: List[Item], topN: Int = 5): List[Recommendation]
+  def name: String
