a9557796caa50f1d6d37d0a36bb95219c6f8399e
Roger Bleggi <roger.bleggi@hotmail.com>
2026-01-13 19:31:05 -0300

    add TicketBuilder class

--- java/ticket-booking/src/main/java/com/rbleggi/ticketbooking/TicketBookingSystem.java
+++ java/ticket-booking/src/main/java/com/rbleggi/ticketbooking/TicketBookingSystem.java
@@ -3,4 +3,35 @@ package com.rbleggi.ticketbooking;
 public class TicketBookingSystem {
 
     public record Ticket(String show, String zone, int seat, String date) {}
+
+    public static class TicketBuilder {
+        private String show = "";
+        private String zone = "";
+        private int seat = 0;
+        private String date = "";
+
+        public TicketBuilder setShow(String show) {
+            this.show = show;
+            return this;
+        }
+
+        public TicketBuilder setZone(String zone) {
+            this.zone = zone;
+            return this;
+        }
+
+        public TicketBuilder setSeat(int seat) {
+            this.seat = seat;
+            return this;
+        }
+
+        public TicketBuilder setDate(String date) {
+            this.date = date;
+            return this;
+        }
+
+        public Ticket buildTicket() {
+            return new Ticket(show, zone, seat, date);
+        }
+    }
 }
