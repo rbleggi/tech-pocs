7ec9d888106c024265d16f2a12e96ef340299aac
Roger Bleggi <roger.bleggi@hotmail.com>
2026-02-01 09:05:18 -0300

    rename Main to GroceryTodoListDemo

---
+++ java/grocery-todo-list/src/main/java/com/rbleggi/grocerytodolist/GroceryTodoListDemo.java
@@ -0,0 +1,8 @@
+package com.rbleggi.grocerytodolist;
+
+public class GroceryTodoListDemo {
+    public static void main(String[] args) {
+        System.out.println("Grocery TODO List Demo");
+    }
+}
+
