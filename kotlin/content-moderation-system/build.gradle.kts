04be688a94f56cd0fa16c29ffa7ed7a0e89e4cc0
Roger Bleggi <roger.bleggi@hotmail.com>
2026-02-18 09:03:47 +0000

    add kotlin content-moderation-system build configuration

---
+++ kotlin/content-moderation-system/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    application
+}
+
+group = "com.rbleggi"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass = "com.rbleggi.contentmoderationsystem.MainKt"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(23)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_23)
+    }
+}
