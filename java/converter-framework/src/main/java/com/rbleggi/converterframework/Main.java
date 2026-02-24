f95354146ae031b368646b1ec87f6bea34c87c35
Roger Bleggi <roger.bleggi@hotmail.com>
2026-02-24 07:03:17 -0300

    update java converter-framework main implementation

--- java/converter-framework/src/main/java/com/rbleggi/converterframework/Main.java
+++ java/converter-framework/src/main/java/com/rbleggi/converterframework/Main.java
@@ -27,21 +27,6 @@ public class Main {
             addressToString.convert(person.address()));
 
     public static void main(String[] args) {
-        Address address = new Address("123 Main St", "Springfield", "12345");
-        Person person = new Person("John Doe", 30, address);
-
-        System.out.println("=== Converter Framework Examples ===\n");
-
-        System.out.println("Address as String: " + addressToString.convert(address));
-        System.out.println("Person as DTO: " + personToDTO.convert(person));
-        System.out.println("Person as String: " + personToString.convert(person));
-
-        System.out.println("\n=== Converter Composition ===\n");
-
-        Converter<Person, String> personToDTOThenToString = personToDTO.then(dto ->
-            String.format("DTO[fullName=%s, age=%d, city=%s]",
-                dto.fullName(), dto.age(), dto.city()));
-
-        System.out.println("Person to DTO to String: " + personToDTOThenToString.convert(person));
+        System.out.println("Converter Framework");
     }
 }
