d10707b046fd2dfff25541969c52875adc841973
Roger Bleggi <roger.bleggi@hotmail.com>
2026-02-24 08:06:44 -0300

    update java observability-framework main implementation

--- java/observability-framework/src/main/java/com/rbleggi/observabilityframework/Main.java
+++ java/observability-framework/src/main/java/com/rbleggi/observabilityframework/Main.java
@@ -7,44 +7,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting Observability Core Framework POC...");
-
-        var msStrategy = new MillisecondsLatencyStrategy();
-        var requestTrackerMs = MetricManager.getTracker("httpRequestLatencyMs", msStrategy);
-
-        System.out.println("\nRecording latencies with Milliseconds Strategy:");
-        for (int i = 1; i <= 5; i++) {
-            var startTime = System.nanoTime();
-            try {
-                Thread.sleep(100 + i * 10L);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            var endTime = System.nanoTime();
-            requestTrackerMs.recordLatency(startTime, endTime);
-            System.out.println("  Recorded latency for request " + i + ": " + msStrategy.calculateLatency(startTime, endTime) + " ms");
-        }
-
-        System.out.println("Average HTTP Request Latency (ms): " + requestTrackerMs.getAverageLatency());
-        System.out.println("All HTTP Request Latencies (ms): " + requestTrackerMs.getAllLatencies());
-
-        var nsStrategy = new NanosecondsLatencyStrategy();
-        var dbQueryTrackerNs = MetricManager.getTracker("dbQueryLatencyNs", nsStrategy);
-
-        System.out.println("\nRecording latencies with Nanoseconds Strategy:");
-        for (int i = 1; i <= 3; i++) {
-            var startTime = System.nanoTime();
-            var dummy = java.util.stream.IntStream.rangeClosed(1, 1000).map(n -> n * 2).sum();
-            var endTime = System.nanoTime();
-            dbQueryTrackerNs.recordLatency(startTime, endTime);
-            System.out.println("  Recorded latency for DB query " + i + ": " + nsStrategy.calculateLatency(startTime, endTime) + " ns");
-        }
-
-        System.out.println("Average DB Query Latency (ns): " + dbQueryTrackerNs.getAverageLatency());
-        System.out.println("All DB Query Latencies (ns): " + dbQueryTrackerNs.getAllLatencies());
-
-        System.out.println("\nAll registered trackers:");
-        MetricManager.getAllTrackerNames().forEach(name -> System.out.println("- " + name));
+        System.out.println("Observability Framework");
     }
 }
 
