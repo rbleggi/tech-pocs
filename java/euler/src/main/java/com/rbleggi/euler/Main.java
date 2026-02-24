c4a38649e13583882da150098ce7fbb404d7a5f1
Roger Bleggi <roger.bleggi@hotmail.com>
2026-02-24 07:11:42 -0300

    update java euler main implementation

--- java/euler/src/main/java/com/rbleggi/euler/Main.java
+++ java/euler/src/main/java/com/rbleggi/euler/Main.java
@@ -2,11 +2,6 @@ package com.rbleggi.euler;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Project Euler Problem 1 ===");
-        P01.main(args);
-        System.out.println("\n=== Project Euler Problem 2 ===");
-        P02.main(args);
-        System.out.println("\n=== Project Euler Problem 3 ===");
-        P03.main(args);
+        System.out.println("Euler");
     }
 }
