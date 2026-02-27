62f7b4f3d1cb4659ca518f95ab52dfbd8f586165
Roger Bleggi <roger.bleggi@hotmail.com>
2026-02-27 08:04:38 -0300

    add java chatbot build configuration

---
+++ java/chatbot/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    application
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+group = "com.rbleggi"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("com.rbleggi.chatbot.Main")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
