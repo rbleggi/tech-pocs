8cabacface2fa272944261d8670806efb895cc5c
Roger Bleggi <roger.bleggi@hotmail.com>
2026-03-17 07:12:43 -0300

    add java credit-risk-assessment test platform launcher dependency

--- ai/java/credit-risk-assessment/build.gradle.kts
+++ ai/java/credit-risk-assessment/build.gradle.kts
@@ -23,6 +23,7 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
