f19e8a9c72c6dde0bfacca1f661c76eaeb6321ac
Roger Bleggi <roger.bleggi@hotmail.com>
2026-05-25 07:42:11 -0300

    add kotlin tax-system build configuration

---
+++ kotlin/tax-system/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    application
+}
+
+group = "com.rbleggi"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass = "com.rbleggi.taxsystem.MainKt"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(25)
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24)
+    }
+}
+tasks.withType<JavaCompile> {
+    options.release.set(24)
+}
